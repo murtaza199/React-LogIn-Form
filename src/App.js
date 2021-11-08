cc29849bc2c1e8fbb698f935a55c355449f307f6
murtaza199 <murtaza.sep@gmail.com>
2021-11-08 21:25:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import "./App.css";
-import {useState} from "react"
-function App(){
-const[status,setStatus]=useState(false)
-  return(
-    <div class="App">{status?<h1>Hello World</h1>:null}
-
-<button onClick={()=>setStatus(!status)}>Show/Hide</button>
-
+import { useState } from "react";
+function App() {
+  const [name, setName] = useState("");
+  const [game, setGame] = useState("");
+  const [tnc, setTnc] = useState(false);
+  function getData(e) {
+    console.log(name,game,tnc)
+    e.preventDefault();
+  }
+  return (
+    <div class="App">
+      <form onSubmit={getData}>
+        <h2>Sports Club Form</h2>
+        <input type="text" placeholder="Enter Your Name" onChange={(e)=>setName(e.target.value)}/>
+        <br />
+        <br />
+        <select onChange={(e)=>setGame(e.target.value)}>
+          <option>Games</option>
+          <option>Cricket</option>
+          <option>Football</option>
+          <option>Tennis</option>
+        </select>
+        <br />
+        <br />
+        <input type="checkbox" onChange={(e)=>setTnc(e.target.checked)}/>
+        <span>Agree Terms and Condition</span>
+        <br />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  )
+  );
 }
-  
 
 export default App;
