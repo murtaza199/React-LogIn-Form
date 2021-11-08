f75a91e3660eb3a4595482d791c44767cbd13c7a
murtaza199 <murtaza.sep@gmail.com>
2021-11-08 21:12:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,34 +1,10 @@
 import "./App.css";
-import { useState } from "react";
+import Profile from './Profile';
 function App() {
-  const [name, setName] = useState("");
-  const [game, setGame] = useState("");
-  const [tnc, setTnc] = useState(false);
-  function getData(e) {
-    console.log(name,game,tnc)
-    e.preventDefault();
-  }
+  
   return (
     <div class="App">
-      <form onSubmit={getData}>
-        <h2>Sports Club Form</h2>
-        <input type="text" placeholder="Enter Your Name" onChange={(e)=>setName(e.target.value)}/>
-        <br />
-        <br />
-        <select onChange={(e)=>setGame(e.target.value)}>
-          <option>Games</option>
-          <option>Cricket</option>
-          <option>Football</option>
-          <option>Tennis</option>
-        </select>
-        <br />
-        <br />
-        <input type="checkbox" onChange={(e)=>setTnc(e.target.checked)}/>
-        <span>Agree Terms and Condition</span>
-        <br />
-        <br />
-        <button type="submit">Submit</button>
-      </form>
+      <Profile/>
     </div>
   );
 }
