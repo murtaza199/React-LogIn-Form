52fd2cc274b0220b7f9607b85c2892b3265a2d61
murtaza199 <murtaza.sep@gmail.com>
2021-11-08 18:23:42 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import "./App.css";
 import {useState} from "react"
 function App() {
 const[data,setData]=useState(null)
+const[print,setPrint]=useState(false)
   function typeData(val){
     setData(val.target.value)
+    setPrint(false)
   }
   return (
     <div className="App">
-      <h2>{data}</h2>
+      {print?
+      <h2>{data}</h2>: null}
       <input type="text" onChange={typeData}></input>
+      <button onClick={()=>setPrint(true)}>Print Data</button>
     </div>
   );
 }
